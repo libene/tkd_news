6e9afb0f4ac796da835cb04da651b2e6eb56bf01
Libene Fernandes <libene@gmail.com>
2018-07-29 21:41:50 -0300

    static events view

--- app/tkd_news/App.js
+++ app/tkd_news/App.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  FlatList,
+  Text,
+  View
+} from 'react-native';
 
-export default class App extends React.Component {
+import Event from './Event';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        {
+          id: 1,
+          title: '12/10 - 14/10 Seletiva Nacional Aberta',
+          place: 'Londrina, Paraná, Brasil',
+        },
+        {
+          id: 2,
+          title: '12/11 - 18/11 Campeonato Mundial Militar',
+          place: 'Rio de Janeiro, Rio de Janeiro, Brasil',
+        },
+        {
+          id: 3,
+          title: '23/11 - 25/11 Copa do Brasil',
+          place: 'São José, Santa Catarina, Brasil',
+        }
+      ]
+    }
+  }
+
+  _renderItem = ({item}) => (
+    <Event
+      id={item.id}
+      title={item.title}
+      place={item.place}
+      thumbnail={item.thumbnail}
+    />
+  );
+
+  _keyExtractor = (item, index) => item.id.toString();
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>TKD News!</Text>
+        <StatusBar
+          barStyle="light-content"
+        />
+        <FlatList
+          data={this.state.events}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
@@ -14,8 +63,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#CCCCCC',
+  }
 });
---
+++ app/tkd_news/Event.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    Image,
+    View
+  } from 'react-native';
+
+export default class Event extends Component {
+    render() {
+        return(
+          <View style={styles.rowContainer}>
+            <View style={styles.rowText}>
+              <Text style={styles.title} numberOfLines={2} ellipsizeMode ={'tail'}>
+                {this.props.title}
+              </Text>
+              <Text style={styles.place} numberOfLines={1} ellipsizeMode ={'tail'}>
+                {this.props.place}
+              </Text>              
+            </View>
+          </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  rowContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    height: 100,
+    padding: 10,
+    marginRight: 10,
+    marginLeft: 10,
+    marginTop: 10,
+    borderRadius: 4,
+    shadowOffset:{  width: 1,  height: 1,  },
+    shadowColor: '#CCC',
+    shadowOpacity: 1.0,
+    shadowRadius: 1,
+  },
+  title: {
+    paddingLeft: 10,
+    paddingTop: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#777'
+  },
+  place: {
+    paddingLeft: 10,
+    marginTop: 5,
+    fontSize: 14,
+    color: '#777'
+  },
+  rowText: {
+    flex: 4,
+    flexDirection: 'column'
+  }
+  });
